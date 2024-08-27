289e454df69f6e740aa690cdc7b8cf46017a2c00
Daria Baranova <darya.baranova@softhub.by>
2024-08-27 17:26:51 +0300

    new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'pre_commit_localization_hooks'
-version = '1.0.3'
+version = '2.0.0'
 description = 'pre-commit hooks for localization error messages'
 
 # Package dependencies
