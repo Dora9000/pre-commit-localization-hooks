d711de1e967ba889df8e48c43c1539de46821705
Daria Baranova <darya.baranova@softhub.by>
2024-08-27 13:14:38 +0300

    add poetry & test data

--- tests/test_missing_error_messages.py
+++ tests/test_missing_error_messages.py
@@ -1,72 +1,33 @@
 import contextlib
 import io
-import os
-import tempfile
+from time import sleep
+
 import pytest
 
 from pre_commit_po_hooks.missing_error_messages import Check
+import pathlib
+
+SKIP_TESTS = False
+
+TEST_DIR = pathlib.Path(__file__).parent.resolve() / "test_data" / "tmp"
+
+PO_FILE = pathlib.Path(__file__).parent.resolve() / "test_data" / "default_en.po"
+PY_FILE = pathlib.Path(__file__).parent.resolve() / "test_data" / "default_errors.py"
 
 
-@pytest.fixture
-def create_po_file(request, faker):
-    def create(contents: list[str] | None = None):
-        if not contents:
-            contents = [faker.pystr(min_chars=1, max_chars=100) for _ in range(2)]
+@pytest.mark.skipif(SKIP_TESTS, reason="no test")
+def test_single_error_file_0(create_po_file, create_errors_file):
+    po_file = create_po_file(from_file=True)
+    errors_file = create_errors_file(from_file=True)
 
-        content = """\n#\nmsgid ""\nmsgstr ""\n\n""" + "\n\n".join(
-            [f'msgid "{c}"\nmsgstr "{c}"' for c in contents]
-        )
-
-        f = tempfile.NamedTemporaryFile(delete=False, mode="w")
-        f.write(content)
-        f.seek(0)
-
-        def teardown():
-            f.close()
-            os.unlink(f.name)
-
-        request.addfinalizer(teardown)
-        return f
-
-    yield create
+    output = Check(repo_directory=str(TEST_DIR), quiet=False, po_filepath=po_file.name, errors_patterns=["*.py"]).execute()
 
 
-@pytest.fixture
-def create_errors_file(request, faker):
-    def create(contents: list[str] | None = None):
-        if not contents:
-            contents = [faker.pystr(min_chars=1, max_chars=100) for _ in range(2)]
-
-        content = """\nclass ErrorsEnum:\n    """ + "\n    ".join(
-            [f'var_{i} = "{c}"' for i, c in enumerate(contents, start=1)]
-        )
-        f = tempfile.NamedTemporaryFile(delete=False, mode="w")
-
-        f.write(content)
-        f.seek(0)
-
-        def teardown():
-            f.close()
-            os.unlink(f.name)
-
-        request.addfinalizer(teardown)
-        return f
-
-    yield create
-
-
-@pytest.mark.skip()
-def test_single_error_file_0(create_po_file, create_errors_file, faker):
-    contents = [faker.pystr(min_chars=1, max_chars=100) for _ in range(10)]
-
-    po_file = create_po_file(contents)
-    errors_file = create_errors_file(contents)
-
-    output = Check(filenames=[errors_file.name], quiet=False, po_filepath=po_file.name).execute()
+    sleep(1000)
     assert output == 0
 
 
-@pytest.mark.skip()
+@pytest.mark.skipif(SKIP_TESTS, reason="no test")
 def test_several_error_files_0(create_po_file, create_errors_file, faker):
     content_1, content_2 = [[faker.pystr(min_chars=1, max_chars=100) for _ in range(10)] for _ in range(2)]
 
@@ -79,7 +40,7 @@ def test_several_error_files_0(create_po_file, create_errors_file, faker):
     assert output == 0
 
 
-@pytest.mark.skip()
+@pytest.mark.skipif(SKIP_TESTS, reason="no test")
 def test_one_error_file_was_updated_1(create_po_file, create_errors_file, faker):
     content = [faker.pystr(min_chars=1, max_chars=100) for _ in range(10)]
 
@@ -100,7 +61,7 @@ def test_one_error_file_was_updated_1(create_po_file, create_errors_file, faker)
     assert output == 0
 
 
-@pytest.mark.skip()
+@pytest.mark.skipif(SKIP_TESTS, reason="no test")
 def test_one_error_file_error_not_exists_1(faker, create_po_file):
     po_file = create_po_file()
     errors_file_name = faker.file_name(extension="py")
@@ -114,7 +75,7 @@ def test_one_error_file_error_not_exists_1(faker, create_po_file):
     assert err_text == f'File {errors_file_name} was not found.\n'
 
 
-@pytest.mark.skip()
+@pytest.mark.skipif(SKIP_TESTS, reason="no test")
 def test_one_error_po_file_not_exists_1(faker, create_errors_file):
     po_file_name = faker.file_name(extension="po")
     errors_file = create_errors_file()
