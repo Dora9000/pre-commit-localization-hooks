881cf8e1b2c5487a30f10c2336ab6d6104e170fd
Daria Baranova <darya.baranova@softhub.by>
2024-08-28 10:28:23 +0300

    delete poetry

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 name = 'pre_commit_localization_hooks'
-version = '1.0.5'
+version = '1.0.6'
 description = 'pre-commit hooks for localization error messages'
 
 # Package dependencies
